ed29a399aa9de609ab6f8dfb1358d5b444702813
André Moraes <andremoraes78@gmail.com>
2019-07-24 14:44:21 -0300

    Primeira versao de classe de dominio

--- src/main/java/com/moraes/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/moraes/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.moraes.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.moraes.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Funcionando";
+	public List listar() {
+
+		Categoria cat1 = new Categoria(1, "Pessoas");
+		Categoria cat2 = new Categoria(2, "Animais");
+
+		List categorias = new ArrayList();
+		categorias.add(cat1);
+		categorias.add(cat2);
+
+		return categorias;
 	}
 }
